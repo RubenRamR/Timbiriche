d2875bf3d6fe478110daad3925659b3062bc7970
ser3154 <159857682+ser3154@users.noreply.github.com>
2025-03-23 16:01:37 -0700

    Add files via upload

---
+++ Main/Run.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package Main;
+
+import View.FrmEditarPerfil;
+import View.FrmRegistro;
+
+/**
+ *
+ * @author rramirez
+ */
+public class Run {
+    
+    public static void main(String[] args) {
+        FrmRegistro fr = new FrmRegistro();
+        fr.setVisible(true);
+//        FrmEditarPerfil fe = new FrmEditarPerfil();
+//        fe.setVisible(true);
+    }
+}
