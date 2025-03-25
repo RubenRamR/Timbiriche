78ca7596c4e2073ef0ede293f936c081efbb8207
QuezadaJL12 <jose.quezada236053@potros.itson.edu.mx>
2025-03-24 20:23:07 -0700

    cambios en los avatares

--- src/main/java/Main/Run.java
+++ src/main/java/Main/Run.java
@@ -2,25 +2,4 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package Main;
 
-<<<<<<< HEAD
-=======
-import View.FrmRegistro;
-
->>>>>>> 1ab0646 (los avatares se pueden seleccionar)
-/**
- *
- * @author rramirez
- */
-public class Run{
-
-    public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World!");
-=======
-        FrmRegistro fr = new FrmRegistro();
-        fr.setVisible(true);
->>>>>>> 1ab0646 (los avatares se pueden seleccionar)
-    }
-}
